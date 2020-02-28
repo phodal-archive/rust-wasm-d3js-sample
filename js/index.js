50e48501f177ce5ec2bd220879d08a5cff513cd4
Phodal Huang <h@phodal.com>
2020-02-28 21:17:17 +0800

    docs: fix typo

--- js/index.js
+++ js/index.js
@@ -1,41 +1,7 @@
 import("../pkg/index.js")
     .then(wasm => {
-        console.log(wasm.Matual)
+        let matual = wasm.Matual.new(5);
+        matual.set(10);
+        console.log(matual.get())
     })
     .catch(console.error);
-
-// export class Matual {
-//     static __construct(ptr) {
-//         return new Matual(ptr);
-//     }
-//
-//     constructor(ptr) {
-//         this.ptr = ptr;
-//     }
-//
-//     free() {
-//         const ptr = this.ptr;
-//         this.ptr = 0;
-//         wasm.__wbg_matual_free(ptr);
-//     }
-//
-//     static new(arg0) {
-//         const ret = wasm.matual_new(arg0);
-//         return Foo.__construct(ret)
-//     }
-//
-//     get() {
-//         const ret = wasm.matual_get(this.ptr);
-//         return ret;
-//     }
-//
-//     set(arg0) {
-//         const ret = wasm.matual_set(this.ptr, arg0);
-//         return ret;
-//     }
-// }
-//
-//
-// let matual = new Matual();
-// matual.get();
-
